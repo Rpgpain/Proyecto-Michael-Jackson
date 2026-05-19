09955f534a5efbe35ae7834596c5e3e2bbbb653d
Rpgpain <romanhernandez2025z@gmail.com>
2026-05-18 22:24:45 -0400

    Creacion de Base de Datos

--- AppMJ/models.py
+++ AppMJ/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Album(models.Model):
-    titulo = models.CharField(max_length=100) 
-    anolanzamiento = models.IntegerField()  
-    portada = models.CharField(max_length=200) 
+    titulo = models.CharField(max_length=100)
+    anio_lanzamiento = models.IntegerField()
+    portada_url = models.CharField(max_length=300)   
+    spotify_url = models.CharField(max_length=300)   
 
     def __str__(self):
         return self.titulo
